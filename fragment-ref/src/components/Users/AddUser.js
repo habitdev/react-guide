9640f56d94a18b93fcb57d608cd11306c769f6bb
habitdev <goodstory16@gmail.com>
2023-06-20 13:39:03 +0900

    Add: state기반의 소스를 ref로 바꿔보기

--- fragment-ref/src/components/Users/AddUser.js
+++ fragment-ref/src/components/Users/AddUser.js
@@ -1,43 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 import Card from '../UI/Card';
 import Button from '../UI/Button';
 import ErrorModal from '../UI/ErrorModal';
 import classes from './AddUser.module.css';
-import Wrapper from '../Helpers/Wrapper';
+// import Wrapper from '../Helpers/Wrapper';
 
 const AddUser = (props) => {
-  const [enteredUsername, setEnteredUsername] = useState('');
-  const [enteredAge, setEnteredAge] = useState('');
+  // useRef: 리렌더링할 필요없이 value만 바꾸고 싶을 경우 사용
+  // useRef은 항상 객체이다
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
+
   const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
-    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+    const enteredName = nameInputRef.current.value;
+    const enteredUserAge = ageInputRef.current.value;
+    if (enteredName.trim().length === 0 || enteredUserAge.trim().length === 0) {
       setError({
         title: 'Invalid input',
         message: 'Please enter a valid name and age (non-empty values).',
       });
       return;
     }
-    if (+enteredAge < 1) {
+    if (+enteredUserAge < 1) {
       setError({
         title: 'Invalid age',
         message: 'Please enter a valid age (> 0).',
       });
       return;
     }
-    props.onAddUser(enteredUsername, enteredAge);
-    setEnteredUsername('');
-    setEnteredAge('');
-  };
-
-  const usernameChangeHandler = (event) => {
-    setEnteredUsername(event.target.value);
-  };
-
-  const ageChangeHandler = (event) => {
-    setEnteredAge(event.target.value);
+    props.onAddUser(enteredName, enteredUserAge);
+    nameInputRef.current.value = '';
+    ageInputRef.current.value = '';
+    // ref를 이용하여 DOM요소를 조작하는 건 매우 드물다
   };
 
   const errorHandler = () => {
@@ -55,21 +53,19 @@ const AddUser = (props) => {
       )}
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
-          <label htmlFor="username">Username</label>
+          <label htmlFor='username'>Username</label>
           <input
-            id="username"
-            type="text"
-            value={enteredUsername}
-            onChange={usernameChangeHandler}
+            id='username'
+            type='text'
+            ref={nameInputRef}
           />
-          <label htmlFor="age">Age (Years)</label>
+          <label htmlFor='age'>Age (Years)</label>
           <input
-            id="age"
-            type="number"
-            value={enteredAge}
-            onChange={ageChangeHandler}
+            id='age'
+            type='number'
+            ref={ageInputRef}
           />
-          <Button type="submit">Add User</Button>
+          <Button type='submit'>Add User</Button>
         </form>
       </Card>
     </React.Fragment>

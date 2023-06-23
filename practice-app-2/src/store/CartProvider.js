24600d5367d90152b27f008addb5985cd2f99e84
habitdev <goodstory16@gmail.com>
2023-06-23 12:05:26 +0900

    Add: 중복항목 수량 업데이트

--- practice-app-2/src/store/CartProvider.js
+++ practice-app-2/src/store/CartProvider.js
@@ -11,10 +11,24 @@ const defaultCartState = {
  * @param {*} action: 코드에 전달되는 액션
  */
 function cartReducer(state, action) {
+  console.log(state, action);
   if (action.type === 'ADD') {
-    // concat(): 추가된 항목을 새로운 배열에 담아 반환해준다
-    const updatedItems = state.items.concat(action.item);
     const updatedAmount = state.totalAmount + action.item.price * action.item.amount;
+
+    const existingCartItemIndex = state.items.findIndex((item) => item.id === action.item.id);
+    const existingCartItem = state.items[existingCartItemIndex];
+    let updatedItems;
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: Number(existingCartItem.amount) + Number(action.item.amount),
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems = state.items.concat(action.item);
+    }
 
     return {
       items: updatedItems,

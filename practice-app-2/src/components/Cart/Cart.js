969077019d3305c4d4137c86c6f386abc3720ada
habitdev <goodstory16@gmail.com>
2023-06-23 11:42:26 +0900

    Add: 장바구니 리스트 컴포넌트 생성

--- practice-app-2/src/components/Cart/Cart.js
+++ practice-app-2/src/components/Cart/Cart.js
@@ -1,16 +1,34 @@
 import React, { useContext } from 'react';
+import CartContext from '../../store/cart-context';
 import classes from './Cart.module.css';
 import Modal from '../UI/Modal';
-import CartContext from '../../store/cart-context';
+import CartItem from './CartItem';
 
 function Cart(props) {
   const cartCtx = useContext(CartContext);
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
+
+  const cartItemRemoveHandler = (id) => {
+    
+  }
+  const cartItemAddHandler = (id) => {
+
+  }
+
   const cartItems = (
-    <ul className={classes['cart-item']}>
+    <ul className={classes['cart-items']}>
       {cartCtx.items.map((item) => {
-        return <li key={item.id}>{item.name}</li>;
+        return (
+          <CartItem
+            key={item.id}
+            name={item.name}
+            amount={item.amount}
+            price={item.price}
+            onAdd={cartItemAddHandler.bind(null, item.id)}
+            onRemove={cartItemRemoveHandler.bind(null, item)}
+          ></CartItem>
+        );
       })}
     </ul>
   );

dabde2929c832aee2dd9eb17152f9e6c505ebd93
habitdev <goodstory16@gmail.com>
2023-06-28 16:39:53 +0900

    Add: 클래스 컴포넌트로 변환해보기

--- redux-app/src/components/Counter.js
+++ redux-app/src/components/Counter.js
@@ -1,4 +1,5 @@
-import { useSelector, useDispatch } from 'react-redux';
+import { Component } from 'react';
+import { useSelector, useDispatch, connect } from 'react-redux';
 import classes from './Counter.module.css';
 
 // 함수형이 아닌 클래스 컴포넌트의 경우 useSelector/useStore가 아니라
@@ -8,11 +9,11 @@ const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
 
-  const incrementHandler = () => {
-    dispatch({ type: 'increment' });
+  const this. = () => {
+    dispatch({ type: 'increment' });this.
   };
   const decrementHandler = () => {
-    dispatch({ type: 'decrement' });
+    dispatch({ type: 'decremethis.nt' });
   };
 
   const toggleCounterHandler = () => {};
@@ -30,4 +31,47 @@ const Counter = () => {
   );
 };
 
+// 클래스 컴포넌트
+/*
+class Counter extends Component {
+  incrementHandler() {
+    this.props.increment()
+  }
+  decrementHandler() {
+    this.props.decrement()
+  }
+
+  toggleCounterHandler() {}
+
+  render() {
+    return (
+      <main className={classes.counter}>
+        <h1>Redux Counter</h1>
+        <div className={classes.value}>{this.props.counter}</div>
+        <div>
+          <button onClick={this.incrementHandler.bind(this)}>Increment</button>
+          <button onClick={this.decrementHandler.bind(this)}>Decrement</button>
+        </div>
+        <button onClick={this.toggleCounterHandler.bind(this)}>Toggle Counter</button>
+      </main>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    counter: state.counter,
+  };
+};
+const mapDispatchToProps = (dispatch) => {
+  return {
+    increment: () => dispatch({ type: 'increment' }),
+    decrement: () => dispatch({ type: 'decrement' }),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter); // 클래스 컴포넌트
+*/
+
 export default Counter;
+

18e5734892e044b3c77957c619a0aa0ba6804d58
habitdev <goodstory16@gmail.com>
2023-06-28 16:24:16 +0900

    Add: 리액트 컴포넌트에서 리덕스 데이터 사용하기

--- redux-app/src/components/Counter.js
+++ redux-app/src/components/Counter.js
@@ -1,12 +1,18 @@
+import { useSelector } from 'react-redux';
 import classes from './Counter.module.css';
 
+// 함수형이 아닌 클래스 컴포넌트의 경우 useSelector/useStore가 아니라
+// connect를 사용한다
+
 const Counter = () => {
+  const counter = useSelector((state) => state.counter);
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );

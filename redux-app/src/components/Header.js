1488155940e29edf8f131cb2bc0fc48ca0e3f247
habitdev <goodstory16@gmail.com>
2023-06-29 13:45:01 +0900

    Add: 로그인, 로그아웃 적용해보기

--- redux-app/src/components/Header.js
+++ redux-app/src/components/Header.js
@@ -1,22 +1,35 @@
+import { authActions } from '../store';
 import classes from './Header.module.css';
+import { useDispatch, useSelector } from 'react-redux';
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const isAuth = useSelector((state) => state.auth.isAuth);
+  const logoutHandler = (event) => {
+    event.preventDefault();
+    dispatch(authActions.logout());
+  };
+
+  const navCont = (
+    <nav>
+      <ul>
+        <li>
+          <a href='/'>My Products</a>
+        </li>
+        <li>
+          <a href='/'>My Sales</a>
+        </li>
+        <li>
+          <button onClick={logoutHandler}>Logout</button>
+        </li>
+      </ul>
+    </nav>
+  );
+
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
-      <nav>
-        <ul>
-          <li>
-            <a href='/'>My Products</a>
-          </li>
-          <li>
-            <a href='/'>My Sales</a>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
-        </ul>
-      </nav>
+      {isAuth && navCont}
     </header>
   );
 };
--- redux-app/src/store/index.js
+++ redux-app/src/store/index.js
@@ -4,12 +4,12 @@ import { createSlice, configureStore } from '@reduxjs/toolkit';
 // ⭐ 반드시 새로운 객체로 설정하여 반환한다
 // configureStore: 여러 개의 리듀서를 하나의 리듀서로 쉽게 합칠 수 있다
 
-const initialState = { counter: 0, showCounter: true };
+const initialCounterState = { counter: 0, showCounter: true };
 // export const INCREMENT = 'increment';
 
 const counterSlice = createSlice({
   name: 'counter',
-  initialState: initialState,
+  initialState: initialCounterState,
   reducers: {
     increment(state) {
       state.counter++;
@@ -26,54 +26,33 @@ const counterSlice = createSlice({
     },
   },
 });
-// reducers안의 메서드는 자동으로 최근 값을 받는다
-// 여기선 기존 상태를 변경할 수 없으므로 위와 같이 작성해도 된다
-// 자동으로 state를 복사하기 때문이다
-/*
-const counterReducer = (state = initialState, action) => {
-  if (action.type === 'increment') {
-    return {
-      counter: state.counter + 1,
-      showCounter: state.showCounter,
-    };
-  }
 
-  if (action.type === 'increase') {
-    return {
-      counter: state.counter + action.amount,
-      showCounter: state.showCounter,
-    };
-  }
-  if (action.type === 'decrement') {
-    return {
-      counter: state.counter - 1,
-      showCounter: state.showCounter,
-    };
-  }
-
-  if (action.type === 'toggle') {
-    return {
-      counter: state.counter,
-      showCounter: !state.showCounter,
-    };
-  }
-
-  return state;
+const initialAuthState = {
+  isAuth: false,
 };
-*/
-
-// counterSlice.actions.toggleCounter
-// 액션을 생성해준다
-// 이런 객체는 이미 type 프로퍼티를 가지고 있다(자동생성)
+const authSlice = createSlice({
+  name: 'auth',
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuth = true;
+    },
+    logout(state) {
+      state.isAuth = false;
+    },
+  },
+});
 
 // const store = createStore(counterSlice.reducer);
 const store = configureStore({
-  reducer: counterSlice.reducer,
-  // reducer: {
-  //   counter: counterSlice.reducer,
-  // }
+  reducer: {
+    counter: counterSlice.reducer,
+    auth: authSlice.reducer,
+  },
 });
 // 리듀서가 여러 개이면 리듀서 map을 만들어 사용한다
 
 export const countActions = counterSlice.actions;
+export const authActions = authSlice.actions;
+
 export default store;

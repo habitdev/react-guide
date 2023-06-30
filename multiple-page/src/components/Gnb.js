6c7a6d76113c18bffda1f0806fc5af3a7b8a1d8f
habitdev <goodstory16@gmail.com>
2023-06-30 15:40:50 +0900

    Add: NavLink 사용해보기

--- multiple-page/src/components/Gnb.js
+++ multiple-page/src/components/Gnb.js
@@ -1,7 +1,16 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // RouterProvider안에서만 작동한다
+import { NavLink } from 'react-router-dom'; // RouterProvider안에서만 작동한다
 
 import classes from './Gnb.module.css';
+
+/* 
+
+  NavLink는 className 속성을 추가할 경우 이는 클래스를 받는 것이
+  아니라 함수를 받는다
+  그 함수는 앵커 태그에 추가되어야 하는 css 클래스 이름을 추가할 것이다
+  isActive: `react-router-dom`가 제공한다, boolean
+  end: url이 to=''안에 있는 글자로 끝나면 true
+ */
 
 function Gnb() {
   return (
@@ -9,10 +18,21 @@ function Gnb() {
       <nav>
         <ul className={classes.list}>
           <li>
-            <Link to='/'>Home</Link>
+            <NavLink
+              to='/'
+              className={({ isActive }) => (isActive ? classes.active : undefined)}
+              end
+            >
+              Home
+            </NavLink>
           </li>
           <li>
-            <Link to='/products'>Products</Link>
+            <NavLink
+              to='/products'
+              className={({ isActive }) => (isActive ? classes.active : undefined)}
+            >
+              Products
+            </NavLink>
           </li>
         </ul>
       </nav>

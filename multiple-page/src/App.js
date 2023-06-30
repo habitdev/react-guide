70af57d5e70cd8b9bd0917df04f91ec283f6c606
habitdev <goodstory16@gmail.com>
2023-06-30 11:52:34 +0900

    Add: 라우트하는 다른 방법1

--- multiple-page/src/App.js
+++ multiple-page/src/App.js
@@ -1,11 +1,23 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Products from './pages/Products';
 
-const router = createBrowserRouter([
+// 다른방법1)
+
+const routeDef = createRoutesFromElements(
+  <Route>
+    <Route path='/' element={<Home/>} />
+    <Route path='/products' element={<Products/>} />
+  </Route>
+)
+
+// 기본)
+/* const router = createBrowserRouter([
   { path: '/', element: <Home /> },
-  { path: '/products', element: <Products /> }
-  ]);
+  { path: '/products', element: <Products /> },
+]); */
+
+const router = createBrowserRouter(routeDef)
 
 function App() {
   return <RouterProvider router={router} />;

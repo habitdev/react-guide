b1fd56cd87e888394d60fb286f3ab7a48417a48f
habitdev <goodstory16@gmail.com>
2023-07-03 09:38:19 +0900

    Add: loader의 특이사항

--- practice-app-3/frontend/src/page/EventsPage.js
+++ practice-app-3/frontend/src/page/EventsPage.js
@@ -25,20 +25,12 @@ export default EventsPage;
 // loader는 메뉴를 이동하기 시작할 때 호출된다
 // 브라우저에서 실행된다
 export async function loader() {
+  // 브라우저에서 실행되므로 브라우저 API를 사용할 수 있다
   const response = await fetch('http://localhost:8080/events');
-  // practice-app-3/backend/routes/events.js에서 가져온다
-  // 가져오는 시간이 늦어지면 리액트 라우터는 데이터를 가져올 때까지 대기한다
+
   if (!response.ok) {
     // ...
   } else {
-    // const resData = await response.json();
-    // return resData.events;
-
-    // new Response('any data', {상세설명})
-    // const res = new Response('any data', { status: 200 });
-
-    // fetch로 데이터를 받아오면 자동으로 response가 돌아오는 것이므로
-    // 아래와 같이 return해도 된다
     return response;
   }
 }

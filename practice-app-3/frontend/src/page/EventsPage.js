fece867f88b7ba8011d0bc034649c88933027b4d
habitdev <goodstory16@gmail.com>
2023-07-03 10:32:05 +0900

    Add: 오류처리1

--- practice-app-3/frontend/src/page/EventsPage.js
+++ practice-app-3/frontend/src/page/EventsPage.js
@@ -9,6 +9,11 @@ import EventsList from '../components/EventsList';
 
 function EventsPage() {
   const data = useLoaderData();
+
+  if (data.isError) {
+    return <p>{data.message}</p>;
+  }
+
   const events = data.events;
 
   return (
@@ -29,7 +34,7 @@ export async function loader() {
   const response = await fetch('http://localhost:8080/events');
 
   if (!response.ok) {
-    // ...
+    return { isError: true, message: 'Could not fetch events' };
   } else {
     return response;
   }

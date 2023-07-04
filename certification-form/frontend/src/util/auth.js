af369a253a6e08637d0cecf5179f470f83747daf
habitdev <goodstory16@gmail.com>
2023-07-04 17:00:48 +0900

    Add: url로 직접 접근할 수 없도록 라우트 보호하기

--- certification-form/frontend/src/util/auth.js
+++ certification-form/frontend/src/util/auth.js
@@ -1,3 +1,5 @@
+import { redirect } from 'react-router-dom';
+
 export function getAuthToken() {
   const token = localStorage.getItem('token');
   return token;
@@ -5,4 +7,14 @@ export function getAuthToken() {
 
 export function tokenLoader() {
   return getAuthToken();
+}
+
+export function checkAuthLoader() {
+  const token = getAuthToken();
+
+  if (!token) {
+    return redirect('/auth');
+  }
+
+  return null; // loader()는 반드시 null 또는 기타 다른 값을 리턴해야 합니다
 }

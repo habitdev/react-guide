8c36253ed46631e5ffba0018905bbdaef887261c
habitdev <goodstory16@gmail.com>
2023-07-04 16:54:35 +0900

    Add: 인증상태에 따른 UI변경

--- certification-form/frontend/src/App.js
+++ certification-form/frontend/src/App.js
@@ -12,12 +12,15 @@ import { action as manipulateEventAction } from './components/EventForm';
 import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
 import AuthenticationPage, { action as authAction } from './pages/Authentication';
 import { action as logoutAction } from './pages/Logout';
+import { tokenLoader } from './util/auth';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
     errorElement: <ErrorPage />,
+    id: 'root', // useRouteLoaderData로 접근 시 사용할 id
+    loader: tokenLoader,
     children: [
       { index: true, element: <HomePage /> },
       {

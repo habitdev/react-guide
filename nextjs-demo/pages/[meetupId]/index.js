6eb0a8c5f179edfeb0ed50d0eba7994668fa87ef
habitdev <goodstory16@gmail.com>
2023-07-05 16:43:37 +0900

    Add: getStaticPaths

--- nextjs-demo/pages/[meetupId]/index.js
+++ nextjs-demo/pages/[meetupId]/index.js
@@ -11,4 +11,40 @@ function MeetupDetail() {
   );
 }
 
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: 'm1',
+        },
+      },
+      {
+        params: {
+          meetupId: 'm2',
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params;
+
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          'https://velog.velcdn.com/images/shinychan95/post/25fe1724-685d-423a-a838-8855a3453939/NodejsReact.jpg',
+        title: 'title 111',
+        description: 'description 111',
+        address: 'address 111',
+      },
+    },
+  };
+}
+
 export default MeetupDetail;

a3e378f672122010860b0ce984db1e357d86a227
habitdev <goodstory16@gmail.com>
2023-08-14 14:17:37 +0900

    Add: 타입스크립트 기본형

---
+++ typescript-react/basics.ts
@@ -0,0 +1,14 @@
+// 기본형: number, boolean, string
+// 자료형: 배열, 객체
+// 함수형: function types, parameters
+
+// 기본형
+let age: number = 24;
+// number를 Number로 적으면 Number객체를 말하므로 반드시 소문자로 적어야 한다.
+
+let userName: string;
+userName = 'Max';
+
+let isIn: boolean = false;
+
+// let hobbies: null;

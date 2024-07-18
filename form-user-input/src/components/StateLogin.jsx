e3264fcd513ddd64cd673d63c026143651956ee3
habitdev <goodstory16@gmail.com>
2024-07-18 16:04:26 +0900

    Add: useEffect와 blur로 폼 검사하기

---
+++ form-user-input/src/components/StateLogin.jsx
@@ -0,0 +1,88 @@
+import { useEffect, useState } from 'react';
+
+export default function Login() {
+  // const [enteredEmail, setEnteredEmail] = useState('');
+  // const [enteredPassword, setEnteredPassword] = useState('');
+  const [enteredValues, setEnteredValues] = useState({
+    email: '',
+    password: '',
+  });
+
+  const [didEdit, setDidEdit] = useState({
+    email: false,
+    password: false,
+  });
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  function handleInputChange(identifier, value) {
+    setEnteredValues((prevValues) => ({
+      ...prevValues,
+      [identifier]: value,
+    }));
+    setDidEdit((prevEdit) => ({
+      ...prevEdit,
+      [identifier]: false,
+    }));
+  }
+
+  function handleInputBlur(identifier, value) {
+    setDidEdit((prevValues) => ({
+      ...prevValues,
+      [identifier]: value,
+    }));
+  }
+
+  const emailIsInvalid = didEdit.email && !enteredValues.email.includes('@');
+
+  // function handleEmailChange(event) {
+  //   setEnteredEmail(event.target.value);
+  // }
+
+  // function handlePasswordChange(event) {
+  //   setEnteredPassword(event.target.value);
+  // }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
+
+      <div className='control-row'>
+        <div className='control no-margin'>
+          <label htmlFor='email'>Email</label>
+          <input
+            id='email'
+            type='email'
+            name='email'
+            onChange={(event) => handleInputChange('email', event.target.value)}
+            onBlur={(event) => handleInputBlur('email', event.target.value)}
+            value={enteredValues.email}
+          />
+          <div className='control-error'>
+            {emailIsInvalid && <p>Please enter a valid email address.</p>}
+          </div>
+        </div>
+
+        <div className='control no-margin'>
+          <label htmlFor='password'>Password</label>
+          <input
+            id='password'
+            type='password'
+            name='password'
+            onChange={(event) =>
+              handleInputChange('password', event.target.value)
+            }
+            value={enteredValues.password}
+          />
+        </div>
+      </div>
+
+      <p className='form-actions'>
+        <button className='button button-flat'>Reset</button>
+        <button className='button'>Login</button>
+      </p>
+    </form>
+  );
+}

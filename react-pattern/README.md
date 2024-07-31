8ca78038d5032e31d2edeba750e618b74931655b
habitdev <goodstory16@gmail.com>
2024-07-31 15:41:17 +0900

    Add: 합성 컴포넌트

---
+++ react-pattern/README.md
@@ -0,0 +1,3 @@
+# 리액트 패턴 및 모범사례
+### compound Component
+
---
+++ react-pattern/src/components/Accordion/Accordion.jsx
@@ -0,0 +1,46 @@
+import { createContext, useContext, useState } from 'react';
+import AccordionItem from './AccordionItem';
+import AccordionTitle from './AccordionTitle';
+import AccordionContent from './AccordionContent';
+
+const AccordionContext = createContext();
+
+export function useAccordionContext() {
+  const ctx = useContext(AccordionContext);
+
+  if (!ctx) {
+    throw new Error(
+      'Accordion-related compnents must be wrapped by <Accordion>'
+    );
+  }
+
+  return ctx;
+}
+
+export default function Accordion({ children, className }) {
+  const [openItemId, setOpenItemId] = useState(false);
+  /* function openItem(id) {
+    setOpenItemId(id);
+  }
+  function closeItem() {
+    setOpenItemId(null);
+  } */
+
+  function toggleItem(id) {
+    setOpenItemId((prevId) => (prevId === id ? null : id));
+  }
+
+  const contextValue = {
+    openItemId,
+    toggleItem,
+  };
+  return (
+    <AccordionContext.Provider value={contextValue}>
+      <ul className={className}>{children}</ul>
+    </AccordionContext.Provider>
+  );
+}
+
+Accordion.Item = AccordionItem;
+Accordion.Title = AccordionTitle;
+Accordion.Content = AccordionContent;
